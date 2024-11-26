ddc72f19a0ad4416ecb264242c802490678ce92c
Laura Nunes <laura.a.sousa.nunes@gmail.com>
2024-11-25 22:31:34 -0400

    feat: cadastrar anamnese geral está funcional

--- public/js/prontuario.js
+++ public/js/prontuario.js
@@ -32,8 +32,218 @@ document.addEventListener("DOMContentLoaded", async () => {
     form3.reset();
   };
 
-  const handleSubmit1 = (e) => {
+  const handleSubmit1 = async (e) => {
     e.preventDefault();
+
+    const endereco = {};
+    const enderecoInputs = document.getElementsByClassName("endereco");
+
+    Array.from(enderecoInputs).forEach((input) => {
+      let value = input.value || null;
+
+      if (input.type === "number") {
+        value = value !== null ? parseInt(value, 10) : null;
+      } else if (input.type === "checkbox") {
+        value = input.checked;
+      }
+
+      endereco[input.name] = value || null;
+    });
+
+    const infoExtra = {};
+    const infoExtraInputs = document.getElementsByClassName("infoExtra");
+
+    Array.from(infoExtraInputs).forEach((input) => {
+      let value = input.value || null;
+
+      if (input.type === "number") {
+        value = value !== null ? parseInt(value, 10) : null;
+      } else if (input.type === "checkbox") {
+        value = input.checked;
+      }
+      
+      infoExtra[input.name] = value || null;
+    });
+
+    const emergencia = {};
+    const emergenciaInputs = document.getElementsByClassName("emergencia");
+
+    Array.from(emergenciaInputs).forEach((input) => {
+      let value = input.value || null;
+
+      if (input.type === "number") {
+        value = value !== null ? parseInt(value, 10) : null;
+      } else if (input.type === "checkbox") {
+        value = input.checked;
+      }
+      
+      emergencia[input.name] = value || null;
+    });
+
+    const historico = {};
+    const historicoInputs = document.getElementsByClassName("historico");
+
+    Array.from(historicoInputs).forEach((input) => {
+      let value = input.value || null;
+
+      if (input.type === "number") {
+        value = value !== null ? parseInt(value, 10) : null;
+      } else if (input.type === "checkbox") {
+        value = input.checked;
+      }
+      
+      historico[input.id] = value || null;
+    });
+
+    const patologia = {};
+    const patologiaInputs = document.getElementsByClassName("patologia");
+
+    Array.from(patologiaInputs).forEach((input) => {
+      let value = input.value || null;
+
+      if (input.type === "number") {
+        value = value !== null ? parseInt(value, 10) : null;
+      } else if (input.type === "checkbox") {
+        value = input.checked;
+      }
+      
+      patologia[input.id] = value || null;
+    });
+
+    const dispProt = {};
+    const dispProtInputs = document.getElementsByClassName("dispProt");
+
+    Array.from(dispProtInputs).forEach((input) => {
+      let value = input.value || null;
+
+      if (input.type === "number") {
+        value = value !== null ? parseInt(value, 10) : null;
+      } else if (input.type === "checkbox") {
+        value = input.checked;
+      }
+      
+      dispProt[input.id] = value || null;
+    });
+
+    const muscDerma = {};
+    const muscDermaInputs = document.getElementsByClassName("muscDerma");
+
+    Array.from(muscDermaInputs).forEach((input) => {
+      let value = input.value || null;
+
+      if (input.type === "number") {
+        value = value !== null ? parseInt(value, 10) : null;
+      } else if (input.type === "checkbox") {
+        value = input.checked;
+      }
+      
+      muscDerma[input.id] = value || null;
+    });
+
+    const endoMeta = {};
+    const endoMetaInputs = document.getElementsByClassName("endoMeta");
+
+    Array.from(endoMetaInputs).forEach((input) => {
+      let value = input.value || null;
+
+      if (input.type === "number") {
+        value = value !== null ? parseInt(value, 10) : null;
+      } else if (input.type === "checkbox") {
+        value = input.checked;
+      }
+      
+      endoMeta[input.id] = value || null;
+    });
+
+    const autoNeuroOnco = {};
+    const autoNeuroOncoInputs = document.getElementsByClassName("autoNeuroOnco");
+
+    Array.from(autoNeuroOncoInputs).forEach((input) => {
+      let value = input.value || null;
+
+      if (input.type === "number") {
+        value = value !== null ? parseInt(value, 10) : null;
+      } else if (input.type === "checkbox") {
+        value = input.checked;
+      }
+      
+      autoNeuroOnco[input.id] = value || null;
+    });
+
+    const cardioCircul = {};
+    const cardioCirculInputs = document.getElementsByClassName("cardioCircul");
+
+    Array.from(cardioCirculInputs).forEach((input) => {
+      let value = input.value || null;
+
+      if (input.type === "number") {
+        value = value !== null ? parseInt(value, 10) : null;
+      } else if (input.type === "checkbox") {
+        value = input.checked;
+      }
+      
+      cardioCircul[input.id] = value || null;
+    });
+
+    const medicamento = {};
+    const medicamentoInputs = document.getElementsByClassName("medicamento");
+
+    Array.from(medicamentoInputs).forEach((input) => {
+      let value = input.value || null;
+
+      if (input.type === "number") {
+        value = value !== null ? parseInt(value, 10) : null;
+      } else if (input.type === "checkbox") {
+        value = input.checked;
+      }
+      
+      medicamento[input.id] = value || null;
+    });
+
+    const infoMed = {};
+    const infoMedInputs = document.getElementsByClassName("infoMed");
+
+    Array.from(infoMedInputs).forEach((input) => {
+      let value = input.value || null;
+
+      if (input.type === "number") {
+        value = value !== null ? parseInt(value, 10) : null;
+      } else if (input.type === "checkbox") {
+        value = input.checked;
+      }
+      
+      infoMed[input.id] = value || null;
+    });
+
+    const estiloVida = {};
+    const estiloVidaInputs = document.getElementsByClassName("estiloVida");
+
+    Array.from(estiloVidaInputs).forEach((input) => {
+      let value = input.value || null;
+
+      if (input.type === "number") {
+        value = value !== null ? parseInt(value, 10) : null;
+      } else if (input.type === "checkbox") {
+        value = input.checked;
+      }
+      
+      estiloVida[input.id] = value || null;
+    });
+
+    const alimentacao = {};
+    const alimentacaoInputs = document.getElementsByClassName("alimentacao");
+
+    Array.from(alimentacaoInputs).forEach((input) => {
+      let value = input.value || null;
+
+      if (input.type === "number") {
+        value = value !== null ? parseInt(value, 10) : null;
+      } else if (input.type === "checkbox") {
+        value = input.checked;
+      }
+      
+      alimentacao[input.id] = value || null;
+    });
 
     const formData = {
       nome: document.getElementsByName("nome")[0].value,
@@ -44,7 +254,50 @@ document.addEventListener("DOMContentLoaded", async () => {
       tel: document.getElementsByName("tel")[0].value || null,
       ultimoAtendimento: document.getElementsByName("ultimoAtendimento")[0].value || null,
       proximoAtendimento: document.getElementsByName("proximoAtendimento")[0].value || null,
+      endereco: endereco,
+      infoExtra: infoExtra,
+      emergencia: emergencia,
+      historico: historico,
+      patologia: patologia,
+      dispProt: dispProt,
+      muscDerma: muscDerma,
+      endoMeta: endoMeta,
+      autoNeuroOnco: autoNeuroOnco,
+      cardioCircul: cardioCircul,
+      medicamento: medicamento,
+      infoMed: infoMed,
+      estiloVida: estiloVida,
+      alimentacao: alimentacao,
     };
+
+    console.log("formData de prontuario.js:", formData);
+
+    try {
+      const method = editMode ? "PUT" : "POST";
+      const url = editMode
+        ? `/paciente/update/${currentEditId}`
+        : "/paciente/create-anamnese-geral";
+  
+      const response = await fetch(url, {
+        method,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+  
+      console.log("response:", response);
+  
+      if (!response.ok){
+        const errorDetails = await response.text();
+        throw new Error(`Erro no envio: ${errorDetails}`);
+      }
+  
+      const result = await response.json();
+      console.log(result.message);
+
+      resetForm1();
+    } catch (error) {
+      console.error("Erro ao enviar ou atualizar paciente:", error);
+    }
   };
 
   const handleSubmit2 = (e) => {
